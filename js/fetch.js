cdc155dd99d79899c9eadf646a5aa748371d54a0
buptsdz <508936331@qq.com>
2024-11-09 03:49:27 +0800

    增加链接跳转统计和追踪;优化统计逻辑,去除自己的访问

--- js/fetch.js
+++ js/fetch.js
@@ -1,9 +1,34 @@
 let lastLikeTime = 0; // 记录上一次点赞的时间戳
 const LIKE_COOLDOWN = 1500; // 冷却时间，单位为毫秒，这里设为2秒
+const true_url = "http://test.sparkflare.cn/sdz_web";
+const test_url = "http://127.0.0.1:60608";
+let base_url = true_url;
 
-// 使用 fetch 获取点赞状态
+// 用户点击链接
+function trackClick(pageName) {
+	console.log(`"click page ${pageName}"`)
+	fetch(`${base_url}/link_click_tracking/`, { // 替换为你的后端API路径
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded', // 使用表单编码类型
+			},
+			body: new URLSearchParams({
+				page_name: pageName, // 传递页面名称作为参数
+			}),
+		})
+		.then(response => response.json())
+		.then(data => {
+			console.log('Success:', data);
+			// 这里可以处理后台返回的数据
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
+}
+
+// 获取点赞状态
 function fetchLikeStatus(itemId, likeIconId, totalLikesId) {
-    fetch(`http://test.sparkflare.cn/sdz_web/get_like_status/?item_id=${itemId}`)
+    fetch(`${base_url}/get_like_status/?item_id=${itemId}`)
         .then(response => {
             if (!response.ok) {
                 return response.json().then(err => { throw new Error(err.message); });
@@ -14,7 +39,7 @@ function fetchLikeStatus(itemId, likeIconId, totalLikesId) {
         .catch(error => showModal("点赞状态请求失败：", error.message));
 }
 
-// 使用 fetch 处理用户点赞
+// 进行点赞操作
 function toggleLike(itemId, likeIconId, totalLikesId) {
     const currentTime = Date.now();
     if (currentTime - lastLikeTime < LIKE_COOLDOWN) {
@@ -24,7 +49,7 @@ function toggleLike(itemId, likeIconId, totalLikesId) {
 
     lastLikeTime = currentTime; // 更新上一次点赞时间
 
-    fetch("http://test.sparkflare.cn/sdz_web/like_toggle/", {
+    fetch(`${base_url}/like_toggle/`, {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
@@ -42,7 +67,7 @@ function toggleLike(itemId, likeIconId, totalLikesId) {
     .catch(error => showModal("点赞失败：", error.message));
 }
 
-// 处理响应
+// 前端处理点赞响应
 function handleResponse(response, likeIconId, totalLikesId) {
     const likeMessage = document.getElementById(totalLikesId);
     if (response.status === "success") {
